930fbbcc68d7d78bc37033774df4d2d34c9a66a4
letunghust <tunglehoctot@gmail.com>
2024-03-14 10:20:32 +0700

    14/3 v1 complete User

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
+// [POST] sign up user 
 const signup = async (req, res) => {
     const { name, surname, phone, email, password, is_admin } = req.body;
 
@@ -11,6 +12,81 @@ const signup = async (req, res) => {
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
+    
+}
+
+//[POST] log in
+const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        // const isMatch = await bcrypt.compare(password, user.password);
+
+        // if (!isMatch) {
+        //     return res.status(401).json({ message: 'Invalid credentials' });
+        // }
+
+       if(password !== user.password) {
+            return res.status(401).json({message: 'Invalid credentials'});
+       }
+
+        res.json({ message: 'Login successful' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+// [PATCH] update user
+const updateUser = async (req, res) => {
+    const userId = req.params.id;
+    const updates = req.body;
+
+    try {
+        const user = await User.findByIdAndUpdate(userId, updates, { new: true });
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+//[GET] get all user 
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+//[GET] get user by id
+const getUserById = async (req, res) => {
+    const userId = req.params.id;
+
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 }
 
 exports.signup = signup;
+exports.login = login;
+exports.getAllUsers = getAllUsers;
+exports.getUserById = getUserById; 
+exports.updateUser = updateUser;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
-const BooksController = require('../controllers/BooksController')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const bcrypt = require('bcrypt');
 const Books = require('../models/Books');
+const User = require('../models/User')
+const BooksController = require('../controllers/BooksController')
 const UserControllers = require('../controllers/UserController')
+
 
 function route(app) {
     app.get('/', BooksController.index);
@@ -14,56 +17,15 @@ function route(app) {
     app.patch("/book/:id", BooksController.updateById)
     app.delete("/book/:id", BooksController.deleteById)
 
-    app.get("/all-uses",  async (req, res) => {
-        try {
-            const users = await User.find();
-            res.json(users);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
-    })
-
-    app.get("user/:id", async (req, res) => {
-    const userId = req.params.id;
-
-    try {
-        const user = await User.findById(userId);
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-        res.json(user);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-})
+    // USER 
+    app.get("/all-users", UserControllers.getAllUsers)
+    app.get("/user/:id", UserControllers.getUserById)
     // Sign up 
     app.post("/signup", UserControllers.signup)
     // Login
-    app.post("/login", async (req, res) => {
-        const { email, password } = req.body;
+    app.post("/login", UserControllers.login)
+    app.patch("/user/:id", UserControllers.updateUser);
     
-        try {
-            const user = await User.findOne({ email });
-    
-            if (!user) {
-                return res.status(401).json({ message: 'Invalid credentials' });
-            }
-    
-            const isMatch = await bcrypt.compare(password, user.password);
-    
-            if (!isMatch) {
-                return res.status(401).json({ message: 'Invalid credentials' });
-            }
-    
-            // Điều này chỉ là một ví dụ, bạn có thể thực hiện xác thực và phát mã thông báo JWT ở đây.
-    
-            res.json({ message: 'Login successful' });
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
-    })
-
-   
 }
 
 module.exports = route;
