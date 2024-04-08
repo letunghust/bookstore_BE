863d0e2d25cfea9fc0967b7d8e1e9740d830c263
letunghust <tunglehoctot@gmail.com>
2024-04-08 10:27:17 +0700

    8_4 v1 delete comment

--- src/controllers/PaymentController.js
+++ src/controllers/PaymentController.js
@@ -7,22 +7,19 @@ const Order = require("../models/Order");
 
 const createPaymenIntent = async (req, res) => {
   try {
-    // Lấy thông tin giỏ hàng và người dùng từ request body
     const { cartId, userId } = req.body;
 
     // Tìm giỏ hàng và người dùng trong cơ sở dữ liệu
     const cart = await Cart.findById(cartId).populate("books.book");
     const user = await User.findById(userId);
 
-    // Tính tổng giá trị đơn hàng
     const totalPrice = cart.books.reduce((total, item) => {
       return total + item.book.price * item.quantity;
     }, 0);
 
-    // Tạo PaymentIntent với Stripe
     const paymentIntent = await stripe.paymentIntents.create({
       amount: totalPrice * 100, // Giá trị tính bằng cent
-      currency: "usd", // Loại tiền tệ
+      currency: "usd", 
       metadata: {
         userId: user._id.toString(),
         cartId: cart._id.toString(),
@@ -39,17 +36,15 @@ const createPaymenIntent = async (req, res) => {
 
 const handlePaymentSuccess = async (req, res) => {
   try {
-    // Lấy thông tin PaymentIntent từ request body
     const { paymentIntentId } = req.body;
 
     // Lấy thông tin PaymentIntent từ Stripe
     const paymentIntent = await stripe.paymentIntents.retrieve(paymentIntentId);
+    console.log('payment intent:  ',paymentIntent);
 
-    // Lấy thông tin người dùng và giỏ hàng từ metadata của PaymentIntent
     const userId = paymentIntent.metadata.userId;
     const cartId = paymentIntent.metadata.cartId;
 
-    // Tìm giỏ hàng và người dùng trong cơ sở dữ liệu
     const cart = await Cart.findById(cartId).populate("books.book");
     const user = await User.findById(userId);
 
@@ -57,11 +52,11 @@ const handlePaymentSuccess = async (req, res) => {
     const order = new Order({
       user: user._id,
       books: cart.books,
-      total_price: paymentIntent.amount / 100, // Giá trị đã được tính bằng cent ở trên
+      total_price: paymentIntent.amount / 100, 
       payment_status: "paid",
       payment_method: "stripe",
       shipping_address: {
-        // Thông tin địa chỉ giao hàng
+        country: ""
       },
     });
 
@@ -73,7 +68,8 @@ const handlePaymentSuccess = async (req, res) => {
 
     res.json({ success: true, order });
   } catch (error) {
-    console.error(error);
+    // console.error(error);
+    console.log(error)
     res
       .status(500)
       .json({ error: "Error when successful payment processing" });
--- src/routes/index.js
+++ src/routes/index.js
@@ -12,7 +12,6 @@ function route(app) {
     // BOOK
     // find by category 
     app.get("/all-books", BooksController.findByCategory)
-    // get a book 
     app.get("/book/:id", BooksController.findById)
     app.post("/upload-book", BooksController.create )
     app.patch("/book/:id", BooksController.updateById)
@@ -21,9 +20,7 @@ function route(app) {
     // USER 
     app.get("/all-users", UserControllers.getAllUsers)
     app.get("/user/:id", UserControllers.getUserById)
-    // Sign up 
     app.post("/signup", UserControllers.signup)
-    // Login
     app.post("/login", UserControllers.login)
     app.patch("/user/:id", UserControllers.updateUser);
     app.get("/fogotpassword", UserControllers.forgotPassword);
